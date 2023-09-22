28cac6b69ed241878c9774471faf7ea6581ee6ee
zbigm <zbigm@OKASHIHQ>
2023-09-21 21:38:41 -0500

    Add project files.

---
+++ BethanySDAWebsite/SiteInfo.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace BethanySDAWebsite
+{
+    public class SiteInfo
+    {
+
+        [JSInvokable]
+        public static string GetTitle()
+        {
+            var title = "Dummy Title";
+
+            return title;
+        }
+    }
+}
