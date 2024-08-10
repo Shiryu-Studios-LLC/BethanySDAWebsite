e7310759ec4af579a39425ea0276b75a26bd78a2
OkashiKami <zbigmoney305@gmail.com>
2024-08-10 00:03:09 -0500

    moveing from api key  to service account

--- HHBW/Utils.cs
+++ HHBW/Utils.cs
@@ -5,82 +5,67 @@ using Google.Apis.Auth.OAuth2;
 using Google.Apis.Drive.v3;
 using Google.Apis.Services;
 using Google.Apis.Drive.v3.Data;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System.Net;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
 
 namespace HHBW
 {
     public class Utils
     {
-        public static async Task<string> GetImageFromDrive(string fileId)
-        {
-            // Load your credentials (client_secret.json) from a file
-            UserCredential credential;
-            using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
-            {
-                credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
-                    GoogleClientSecrets.Load(stream).Secrets,
-                    new[] { DriveService.Scope.DriveReadonly },
-                    "user",
-                    CancellationToken.None
-                );
-            }
+        private readonly IConfiguration _configuration;
 
-            // Create Drive API service
-            var service = new DriveService(new BaseClientService.Initializer()
+        // Constructor with dependency injection for IConfiguration
+        public Utils(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        private T? GetGoogleService<T>()
+        {
+            var credential = GoogleCredential.FromFile("credentials.json");
+            var initializer = new BaseClientService.Initializer()
             {
                 HttpClientInitializer = credential,
-                ApplicationName = "Google Drive API Sample",
-            });
-
-            // Define request parameters
-            var request = service.Files.List();
-            request.PageSize = 10;
-            request.Fields = "nextPageToken, files(id, name)";
-
-
-            // List files
-            var result = await request.ExecuteAsync();
-            foreach (var file in result.Files)
+                ApplicationName = "Bethany SDA Website"
+            };
+            T? service = default;
+            if (typeof(T).Equals(typeof(DriveService)))
             {
-                Console.WriteLine($"File Name: {file.Name}, File ID: {file.Id}");
+                credential.CreateScoped(DriveService.Scope.Drive);
+                service = (T)(object)new DriveService(initializer);
             }
+            
+            return service;
+        }
 
+        public async Task<string> GetImageFromDrive(string fileId)
+        {
+            try
+            {
+                var service = GetGoogleService<DriveService>();                
+                // Define request parameters to get the file metadata
+                var request = service.Files.Get(fileId);
+                request.Fields = "id, name, mimeType, webContentLink";
+
+                // Execute the request and get the file metadata
+                var file = await request.ExecuteAsync();
+
+                // Check if the file is an image
+                if (file.MimeType.StartsWith("image/"))
+                {
+                    // Google Drive's direct download link format
+                    string downloadUrl = $"https://drive.google.com/uc?export=download&id={file.Id}";
+                    return downloadUrl;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+                return string.Empty;
+            }
             return string.Empty;
-        }
-    }
-    public static  class DateTimeExtensions
-    {
-
-        public enum Months : int
-        {
-            Jan = 1,
-            Feb,
-            Mar,
-            Apr,
-            May,
-            Jun,
-            Jul,
-            Aug,
-            Sep,
-            Oct,
-            Nov,
-            Dec,
-        }
-        public enum AMPM { AM, PM }
-        // Extension method to format DateTime in a custom way
-        public static DateTime ToCustomFormat(this DateTime dateTime, Months month = default, int day = default, int year = default, int hour = default, int minutes = default, AMPM aMPM = default)
-        {
-
-            if (month == default) month = (Months)DateTime.Now.Month;
-            if(day == default) day = DateTime.Now.Day;
-            if(year == default) year = DateTime.Now.Year;
-
-            if(hour == default) hour = DateTime.Now.Hour;
-            if(minutes == default) minutes = DateTime.Now.Minute;
-            if (aMPM == default) aMPM = Enum.Parse<AMPM>(DateTime.Now.ToString("tt"));
-
-            string format = "M/d/yyyy h:mmtt"; // Specify the format that matches the input string
-            CultureInfo provider = CultureInfo.InvariantCulture;
-            return DateTime.ParseExact($"{(int)month}/{day}/{year} {hour}:{minutes.ToString("D2")}{aMPM.ToString().ToLower()}", format, provider);
         }
     }
 }
